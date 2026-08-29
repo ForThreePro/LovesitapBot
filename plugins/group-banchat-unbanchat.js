11a272e4163755de1fff83969649224b20b82fa7
YALLICO OWNER <yallico2024@gmail.com>
2026-08-28 23:02:22 -0500

    Actualizar group-banchat-unbanchat.js

--- plugins/group-banchat-unbanchat.js
+++ plugins/group-banchat-unbanchat.js
@@ -4,16 +4,16 @@ let handler = async (m, { conn, isOwner, isROwner, command }) => {
   let type = command.toLowerCase()
 
   if (!(isOwner || isROwner)) {
-    return conn.reply(m.chat, `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Solo owners pueden usar este comando*`, m)
+    return conn.reply(m.chat, `🍰 𓆩 ***𝗟𝗢𝗩𝗘𝗦𝗜𝗧𝗔𝗣 𝗕𝗢𝗧*** 𓆪 🌸\n\n🍰 *Solo owners pueden usar este comando* 🍜`, m)
   }
 
   switch (type) {
     case 'banchat': case 'banearchat':
-      if (chat.isBanned) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Este chat ya se encuentra baneado.*`)
+      if (chat.isBanned) return m.reply(`🍰 𓆩 ***𝗟𝗢𝗩𝗘𝗦𝗜𝗧𝗔𝗣 𝗕𝗢𝗧*** 𓆪 🌸\n\n🍰 *Este chat ya se encuentra baneado.* 🍜`)
       chat.isBanned = true
-      await conn.reply(m.chat, `🐱 𓆩 𝗖𝗛𝗔𝗧 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐱
+      await conn.reply(m.chat, `🍰 𓆩 𝗖𝗛𝗔𝗧 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🌸
 
-.⃟𖥔 ݁. 𖦹˙— \`\`BAN\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`BAN\`\` —˙𖦹.🍜꒷
 
 🚫 *El bot ha sido desactivado en este grupo*
 💤 *No responderé a ningún comando hasta que sea desbloqueado*
@@ -21,23 +21,25 @@ let handler = async (m, { conn, isOwner, isROwner, command }) => {
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [m.sender] })
+*Powered by*: ***Lovesitap Bot*** 💎
+> "Pausando la pastelería por un rato" 🍰`, m, { mentions: [m.sender] })
       break
 
     case 'unbanchat': case 'desbanearchat':
-      if (!chat.isBanned) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Este chat no está baneado.*`)
+      if (!chat.isBanned) return m.reply(`🍰 𓆩 ***𝗟𝗢𝗩𝗘𝗦𝗜𝗧𝗔𝗣 𝗕𝗢𝗧*** 𓆪 🌸\n\n🍰 *Este chat no está baneado.* 🍜`)
       chat.isBanned = false
-      await conn.reply(m.chat, `🐱 𓆩 𝗖𝗛𝗔𝗧 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐱
+      await conn.reply(m.chat, `🍰 𓆩 𝗖𝗛𝗔𝗧 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🌸
 
-.⃟𖥔 ݁. 𖦹˙— \`\`UNBAN\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`UNBAN\`\` —˙𖦹.🍜꒷
 
-😼 *El bot vuelve a estar activo en este grupo*
-🍕 *Ya pueden utilizar todos los comandos con normalidad*
+🌸 *El bot vuelve a estar activo en este grupo*
+🍰 *Ya pueden utilizar todos los comandos con normalidad*
 
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [m.sender] })
+*Powered by*: ***Lovesitap Bot*** 💎
+> "La pastelería abre de nuevo" 🍰`, m, { mentions: [m.sender] })
       break
 
     default:
