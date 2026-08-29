a60f781e40d17fc57b35648b71b85ed949a113a8
YALLICO OWNER <yallico2024@gmail.com>
2026-08-28 22:53:12 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,14 +2,13 @@ import os from 'os'
 import { performance } from 'perf_hooks'
 
 let handler = async (m, { conn, usedPrefix }) => {
-  let loadMsg = await conn.reply(m.chat, `🐱 𓆩 𝗖𝗔𝗥𝗚𝗔𝗡𝗗𝗢 𝗠𝗘𝗡𝗨 𓆪 🐱\n\n⏳ *Espere un momento...*\n> Cargando sistema Garfield...`, m)
+  let loadMsg = await conn.reply(m.chat, `🍰 𓆩 𝗖𝗔𝗥𝗚𝗔𝗡𝗗𝗢 𝗠𝗘𝗡𝗨 𓆪 🌸\n\n⏳ *Espere un momento...*\n> Cargando sistema Lovesitap... 🍜`, m)
 
   let taguser = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
 
   // Rota las 2 fotos
   const images = [
-    'https://files.evogb.win/QFXQtu.jpg',
-    'https://files.evogb.win/E2yVdA.jpg'
+    'https://files.evogb.win/rJ2MBG.jpg'
   ]
   let img = { url: images[Math.floor(Math.random() * images.length)] }
 
@@ -21,8 +20,6 @@ let handler = async (m, { conn, usedPrefix }) => {
   let end = performance.now()
   let ping = (end - start).toFixed(2)
 
-  let owner = global.owner?.[0]?.[0] || '51927174369'
-  let ownerTag = `@${owner}`
   let numBot = conn.user.jid.split('@')[0]
 
   let help = Object.values(global.plugins).filter(p => p.help &&!p.disabled)
@@ -47,15 +44,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     nsfw: 'NSFW', audio: 'AUDIO', prem: 'PREM', otros: 'OTROS'
   }
 
-  let menu = `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n`
+  let menu = `🍰 𓆩 ***𝗟𝗢𝗩𝗘𝗦𝗜𝗧𝗔𝗣 𝗕𝗢𝗧*** 𓆪 🌸\n\n`
   menu += `⤷ ┇ 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 ﹒ 3.0 PREM ：✿ 。\n`
   menu += `꒰ ◞⁺⊹ ．estado: *EN LINEA* • ${_uptime}\n\n`
   menu += ` ꒱ ׁ. ᘏ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗔𝗖𝗧𝗜𝗩𝗢 ׅ 𝆬 ָ֢ ෆ\n`
-  menu += `🐱 ࣪ ꕀ @${taguser.split('@')[0]}. ˚. ᵎᵎ\n`
-  menu += `> *Bienvenido al sistema Garfield*\n\n`
+  menu += `🍰 ࣪ ꕀ @${taguser.split('@')[0]}. ˚. ᵎᵎ\n`
+  menu += `> *Bienvenido al sistema Lovesitap* 💎\n\n`
   menu += `──🍃 *INFORMACION DEL BOT* ╏ 💚\n`
   menu += `*Usuarios*: ${totalreg} | *Comandos*: ${totalcmd}\n`
-  menu += `*Owner*: ${ownerTag}\n`
   menu += `*Numero*: +${numBot}\n\n`
   menu += ` ׅ 🍃 : 𝖲𝖨𝖲𝖳𝖤𝖬𝖠 ﹙ 🌿 ﹚\n`
   menu += `> ﹒ RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb / ${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)}gb\n`
@@ -75,19 +71,17 @@ let handler = async (m, { conn, usedPrefix }) => {
   }
 
   menu += `━━━━━━━━━━━\n`
-  menu += `🐱 ***Garfield Bot Oficial*** 🐱\n`
-  menu += `*Owner*: ${ownerTag}\n`
+  menu += `🍰 ***Lovesitap Bot*** 🌸\n`
   menu += `*Contacto*: +${numBot}\n`
   menu += `*Version*: 3.0 PREM\n`
-  menu += `*Power*: Nivel Garfield\n`
-  menu += `> "Odio los lunes... pero amo la lasaña" 🍕\n`
+  menu += `*Power*: Nivel Lovesitap\n`
+  menu += `> "Los postres son la respuesta a todo" 🍰\n`
   menu += `━━━━━━━━━━━`
 
-  // await conn.sendMessage(m.chat, { delete: loadMsg.key }) <-- ELIMINADO
   await conn.sendMessage(m.chat, {
     image: img,
     caption: menu,
-    mentions: [taguser, owner]
+    mentions: [taguser]
   }, { quoted: m })
 }
 
