c9e5a22120c0fb4d597dc2fd37d86d5e13871852
YALLICO OWNER <yallico2024@gmail.com>
2026-09-22 16:17:11 -0500

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -81,7 +81,7 @@ const handler = async (m, { isOwner, isAdmin, conn, participants, args }) => {
 > "Invocando a todos para compartir postres" 🍰`
 
     // IMAGEN FIJA - Solo cambié la URL
-    const imageUrl = { url: 'https://files.evogb.win/pyhshs.jpg' }
+    const imageUrl = { url: 'https://files.evogb.win/jaSA9q.jpg' }
 
     await conn.sendMessage(m.chat, {
       image: imageUrl,
